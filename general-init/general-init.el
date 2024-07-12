f5db9cbf155aa03242a5321732d0801756b6b3b0
Matt Teichman <teichman@uchicago.edu>
2024-07-12 16:21:44 -0500

    add machine-specific init projects

---
+++ general-init/general-init.el
@@ -0,0 +1,9 @@
+;; cd to homedir
+(cd "~")
+
+;; Turn off mouse interface early in startup to avoid momentary redisplay
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1)) ;turn off menu bar
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1)) ;turn off tool bar
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1)) ;turn off vertical scroll bar
+(when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1)) ;turn off horiz scroll bar
+
