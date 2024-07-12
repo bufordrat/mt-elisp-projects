201d314115bbfc6c62abbd7d7cf5979d63a46d18
Matt Teichman <teichman@uchicago.edu>
2024-07-12 16:32:07 -0500

    tried removing the darwin path thing, hopefully it doesn't break anything

--- general-init/general-init.el
+++ general-init/general-init.el
@@ -7,3 +7,9 @@
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1)) ;turn off vertical scroll bar
 (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1)) ;turn off horiz scroll bar
 
+;; systems
+;; on Linux, super is Meta
+(cond
+  ((eq 'gnu/linux system-type)
+   (setq x-super-keysym 'meta)))
+
