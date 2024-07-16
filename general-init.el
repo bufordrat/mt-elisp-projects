0fd2f2eb9a06625e4d6efc52f0d805a29726ddfb
Matt Teichman <teichman@uchicago.edu>
2024-07-16 11:07:25 -0500

    move nerd icons from scratchpad-init back into general-init

--- general-init.el
+++ general-init.el
@@ -119,3 +119,14 @@
 (dolist
     (f '(run-tint tint-mode tint-eval tint-eval-at-point))
   (autoload f "tint" nil t))
+
+;; modeline
+(add-hook 'after-init-hook #'doom-modeline-mode)
+
+;; dired
+(require 'nerd-icons)
+(require 'nerd-icons-dired)
+(add-hook 'dired-mode-hook #'nerd-icons-dired-mode)
+
+;; (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+(with-demoted-errors "%s" (diredfl-global-mode +1))
