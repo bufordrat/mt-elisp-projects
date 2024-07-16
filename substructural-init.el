039864915f7e107c0f6555d9ec58c5ec3f088535
Matt Teichman <teichman@uchicago.edu>
2024-07-16 09:50:57 -0500

    fix m+ 1mn font size on substructural

--- substructural-init.el
+++ substructural-init.el
@@ -1,5 +1,5 @@
 ;; font stuff
-(defconst matt-default-font "M+ 1mn-12")
+(defconst matt-default-font "M+ 1mn-14")
 (add-to-list 'default-frame-alist (cons 'font matt-default-font))
 (set-face-font 'default matt-default-font)
 
